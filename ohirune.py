abd8084a13cecfa9e8ffd15717c4081c209abce9
tm610 <tomotaro.muto@gmail.com>
2020-05-08 16:04:06 +0900

    こみっと

---
+++ ohirune.py
@@ -0,0 +1,15 @@
+import time
+
+def ohirune():
+    print("neru")
+    time.sleep(10)
+    print("okita")
+    time.sleep(10)
+    print("nidone shita")
+
+def ushi():
+    print('ushi desu')
+    print('kaeru desu')
+    print('Ushironihitonanteinai')
+
+ohirune()
